0f587941abe5f079da831ea6accd2e6c9ae3431c
石部達也 <tatsuya_i7@r.recruit.co.jp>
2015-06-21 16:34:13 +0900

    オートレイアウト

--- YumYum/YumYum/TopViewController.swift
+++ YumYum/YumYum/TopViewController.swift
@@ -7,31 +7,46 @@
 //
 
 import UIKit
+import GoogleMobileAds
+
 
 class TopViewController: UIViewController {
 
+    @IBOutlet weak var bannerView: GADBannerView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.setBannerView()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
     }
 
     @IBAction func slotTapped(sender: AnyObject) {
         var pc = SlotViewController(nibName: "SlotViewController", bundle: nil)
         
-        self.navigationController?.pushViewController(pc, animated: true);
+        self.navigationController?.pushViewController(pc, animated: true)
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
 
     @IBAction func countTapped(sender: AnyObject) {
         var pc = CountViewController(nibName: "CountViewController", bundle: nil)
         
-        self.navigationController?.pushViewController(pc, animated: true);
+        self.navigationController?.pushViewController(pc, animated: true)
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
 
     }
     @IBAction func bombTapped(sender: AnyObject) {
         var pc = BombViewController(nibName: "BombViewController", bundle: nil)
         
-        self.navigationController?.pushViewController(pc, animated: true);
-
+        self.navigationController?.pushViewController(pc, animated: true)
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    func setBannerView() {
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.loadRequest(GADRequest())
     }
 }
