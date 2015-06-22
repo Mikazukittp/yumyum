ad719496a2085c60a522b718926ed6203c1b629a
石部達也 <tatsuya_i7@r.recruit.co.jp>
2015-06-22 23:37:20 +0900

    一旦保存

--- YumYum/Pods/Target Support Files/Pods/Pods-environment.h
+++ YumYum/Pods/Target Support Files/Pods/Pods-environment.h
@@ -12,3 +12,9 @@
 #define COCOAPODS_VERSION_MINOR_Google_Mobile_Ads_SDK 3
 #define COCOAPODS_VERSION_PATCH_Google_Mobile_Ads_SDK 1
 
+// Spring
+#define COCOAPODS_POD_AVAILABLE_Spring
+#define COCOAPODS_VERSION_MAJOR_Spring 1
+#define COCOAPODS_VERSION_MINOR_Spring 0
+#define COCOAPODS_VERSION_PATCH_Spring 3
+
