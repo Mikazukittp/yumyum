928c4b358f90c79afa2a7e5b9b64e0e9851bd106
石部達也 <tatsuya_i7@r.recruit.co.jp>
2015-06-25 00:03:42 +0900

    インフォ画面への遷移・デザイン修正

---
+++ YumYum/YumYum/InfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InfoViewController.swift
+//  YumYum
+//
+//  Created by 石部達也 on 2015/06/24.
+//  Copyright (c) 2015年 石部達也. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Info"
+    }
+}
